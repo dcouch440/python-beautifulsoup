1dfcb9c0eba1f79af0c9021c00d5dd25933fe50e
dcouch440 <dcouch440@gmail.com>
2021-06-12 18:15:10 -0700

    add tag iterator

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ with open('home.html', 'r') as html_file:
   content = html_file.read()
 
   soup = BeautifulSoup(content, 'lxml')
-  print(soup.prettify)
+  courses_html_tags = soup.find_all('h5')
+  for course in courses_html_tags:
+    print(course.text)
