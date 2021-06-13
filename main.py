d4df86937c0e698cfd0a04a7791c03e526f58d9d
dcouch440 <dcouch440@gmail.com>
2021-06-12 18:24:29 -0700

    add string interpolation

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ with open('home.html', 'r') as html_file:
   for course in course_cards:
     course_name = course.h5.text
     course_price = course.a.text.split()[-1]
-    print(course_price)
+    # f promotes string interpolation
+    print(f'{course_name} costs {course_price}')
